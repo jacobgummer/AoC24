98f128824373cf3c1e6c8941bf5efc6ed1e68e4f
Jacob Siegumfeldt <jacobi7th@gmail.com>
2024-12-01 08:36:41 +0100

    December 1st.

--- Dec1/task1.py
+++ Dec1/task1.py
@@ -1,9 +1,15 @@
 import numpy as np
-import pandas as pd
-import re
-import functools
 
 input = open('./input.txt', 'r')
 lines = input.read().splitlines()
 input.close()
 
+fst, snd = [], []
+for line in lines:
+    nums = [int(x) for x in line.split()]
+    fst.append(nums[0])
+    snd.append(nums[1])
+
+fst, snd = np.array(sorted(fst)), np.array(sorted(snd))
+res = np.sum(np.abs(fst - snd))
+print(res)
