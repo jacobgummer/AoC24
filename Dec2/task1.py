567fb170694be443f737cec2032cbcf2f3916ce5
Jacob Siegumfeldt <jacobi7th@gmail.com>
2024-12-02 11:46:04 +0100

    2 December

--- Dec2/task1.py
+++ Dec2/task1.py
@@ -1,8 +1,29 @@
-import numpy as np
-import pandas as pd
-import re
-import functools
-
-input = open('./input.txt', 'r')
+input = open('./test.txt', 'r')
 lines = input.read().splitlines()
 input.close()
+
+for i in range(len(lines)):
+    lines[i] = [int(x) for x in lines[i].split()]
+
+def analyze_report(report):
+    n = len(report)
+    fst_diff = report[1] - report[0]
+    if fst_diff >= -3 and fst_diff <= -1: # decreasing
+        for i in range(1, n-1):
+            diff = report[i+1] - report[i]
+            if diff < -3 or diff > -1: return 0
+        return 1
+    elif fst_diff >= 1 and fst_diff <= 3: # increasing
+        for i in range(1, n-1):
+            diff = report[i+1] - report[i]
+            if diff < 1 or diff > 3: return 0
+        return 1        
+    return 0
+
+safe_reports = 0
+for report in lines:
+    safe = analyze_report(report)
+    if safe == 1: print(f'{report}')
+    safe_reports += safe
+    
+print(safe_reports)
