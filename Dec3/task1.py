1ceb1f180e666e180968d1c25564665fcf06a9c8
Jacob Siegumfeldt <jacobi7th@gmail.com>
2024-12-03 09:03:33 +0100

    3 December

--- Dec3/task1.py
+++ Dec3/task1.py
@@ -1,8 +1,34 @@
 import numpy as np
-import pandas as pd
 import re
-import functools
+import sys
 
-input = open('./input.txt', 'r')
-lines = input.read().splitlines()
+file = ''
+
+if len(sys.argv) != 2:
+    print("Usage: python3 task1.py [i|t]")
+    sys.exit(1)
+
+argument = sys.argv[1]
+
+if argument == "i":
+    file = 'input'
+elif argument == "t":
+    file = 'test'
+else:
+    print("Invalid argument. Use 'i' or 't'.")
+    sys.exit(1)
+
+input = open(f'./{file}.txt', 'r')
+lines = input.read()
 input.close()
+
+regex1 = r'mul\([0-9]+,[0-9]+\)'
+test = [re.findall(regex1, line) for line in lines]
+test = re.findall(regex1, lines)
+
+regex2 = r'[0-9]+(?=,)'
+regex3 = r'(?<=,)[0-9]+'
+
+arr1 = np.array([int(x) for x in [re.findall(regex2, match)[0] for match in test]])
+arr2 = np.array([int(x) for x in [re.findall(regex3, match)[0] for match in test]])
+print(np.sum(arr1 * arr2))
