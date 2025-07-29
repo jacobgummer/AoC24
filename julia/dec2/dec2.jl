f207bd6ff3305cfa0483c8bb0404d7d8c26afceb
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-07-29 23:56:52 +0200

    Split over multiple lines for readability

--- julia/dec2/dec2.jl
+++ julia/dec2/dec2.jl
@@ -1,13 +1,19 @@
 include("../utils.jl")
 using .Utils: get_lines
 
-which_task = length(ARGS) >= 1 ? strip(ARGS[1]) : error("specify which task to do (t1 or t2)")
+which_task = length(ARGS) >= 1 ?
+             strip(ARGS[1]) :
+             error("specify which task to do (t1 or t2)")
 is_test = false
 if length(ARGS) == 2
-  is_test = ARGS[2] == "test" ? true : error("snd argument must be \"test\" to do test")
+  is_test = ARGS[2] == "test" ?
+            true :
+            error("snd argument must be \"test\" to do test")
 end
 
-lines = is_test ? get_lines("test.txt") : get_lines("input.txt")
+lines = is_test ?
+        get_lines("test.txt") :
+        get_lines("input.txt")
 
 function task1()
   processed = [split(l) for l in lines]
