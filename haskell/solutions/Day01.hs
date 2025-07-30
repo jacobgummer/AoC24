5b63b360f83b1a5d0973432f195e92f74c804270
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-07-30 15:02:40 +0200

    Start from scratch

--- haskell/solutions/Day01.hs
+++ haskell/solutions/Day01.hs
@@ -9,19 +9,19 @@ main = do
     1 -> do
       input <- readInputLines "data/inputs/day01.txt"
       case args of
-        ["1"] -> print $ part1 input
-        ["2"] -> print $ part2 input
-        _ -> putStrLn "Usage: day01 -- 1|2 [-t]"
+        ["t1"] -> print $ part1 input
+        ["t2"] -> print $ part2 input
+        _ -> putStrLn "Usage: ./day 01 t1|t2 [-t]"
     2 -> do
       input <- readInputLines "data/tests/day01.txt"
       case args of
-        ["1", "-t"] -> print $ part1 input
-        ["2", "-t"] -> print $ part2 input
-        _ -> putStrLn "Usage: day01 -- 1|2 [-t]"
-    _ -> putStrLn "Usage: day01 -- 1|2 [-t]"
+        ["t1", "-t"] -> print $ part1 input
+        ["t2", "-t"] -> print $ part2 input
+        _ -> putStrLn "Usage: ./day 01 t1|t2 [-t]"
+    _ -> putStrLn "Usage: ./day 01 t1|t2 [-t]"
 
-part1 :: [T.Text] -> T.Text
-part1 = head
+part1 :: [T.Text] -> Int
+part1 input = undefined
 
 part2 :: [T.Text] -> Int
-part2 = T.length . T.concat
+part2 input = undefined
